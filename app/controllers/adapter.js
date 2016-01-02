79baaa89b4aa1bfdc3f2b680789da58a39d1b873
jeffreybiles <bilesjeffrey@gmail.com>
2016-01-02 13:03:52 -0600

    Can choose between models in a more intuitive fashion

--- app/controllers/adapter.js
+++ app/controllers/adapter.js
@@ -3,16 +3,17 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   urlTracker: Ember.inject.service(),
   urls: {},
-  modelName: 'taco-taco',
+  modelName: 'taco',
   actions: {
-    outputUrls(){
+    outputUrlsFor(modelName){
+      this.set('modelName', modelName)
       this.findUrls()
     }
   },
-  errorMessage: '',
   findUrls(){
     try {
       var modelName = this.get('modelName')
+      // TODO: Turn these into a promise hash after the update, instead of just chained
       var prom = this.store.createRecord(modelName, {}).save()
       prom.then((taco)=>{
         taco.set('cost', 3)
@@ -21,16 +22,16 @@ export default Ember.Controller.extend({
         taco.deleteRecord()
         taco.save()
       }).then(()=>{
-        return this.store.findAll(modelName)
-      }).then(()=>{
         return this.store.findRecord(modelName, 1)
+      }).then(()=>{
+        return this.store.findAll(modelName)
       }).then(()=>{
         return this.store.query(modelName, {'tasty': true})
       }).then(()=>{
         return this.store.queryRecord(modelName, {'tasty': true})
       }).then(()=>{
         this.set('urls', this.get('urlTracker.urls'))
-        this.set('errorMessage', '')
+        this.propertyDidChange('urls')
       })
     } catch(e){
       this.set('errorMessage', e.message)
