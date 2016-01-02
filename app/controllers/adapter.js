179a2dd8e44cd55165471c223dd351a494662bb0
jeffreybiles <bilesjeffrey@gmail.com>
2016-01-02 09:53:00 -0600

    * generified modelName * error message for when model doesn't exist

--- app/controllers/adapter.js
+++ app/controllers/adapter.js
@@ -3,30 +3,38 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   urlTracker: Ember.inject.service(),
   urls: {},
+  modelName: 'taco-taco',
   actions: {
     outputUrls(){
       this.findUrls()
     }
   },
+  errorMessage: '',
   findUrls(){
-    var prom = this.store.createRecord('taco-taco', {}).save()
-    prom.then((taco)=>{
-      taco.set('cost', 3)
-      return taco.save()
-    }).then((taco)=>{
-      taco.deleteRecord()
-      taco.save()
-    }).then(()=>{
-      return this.store.findAll('taco-taco')
-    }).then(()=>{
-      return this.store.findRecord('taco-taco', 1)
-    }).then(()=>{
-      return this.store.query('taco-taco', {'tasty': true})
-    }).then(()=>{
-      return this.store.queryRecord('taco-taco', {'tasty': true})
-    }).then(()=>{
-      this.set('urls', this.get('urlTracker.urls'))
-    })
+    try {
+      var modelName = this.get('modelName')
+      var prom = this.store.createRecord(modelName, {}).save()
+      prom.then((taco)=>{
+        taco.set('cost', 3)
+        return taco.save()
+      }).then((taco)=>{
+        taco.deleteRecord()
+        taco.save()
+      }).then(()=>{
+        return this.store.findAll(modelName)
+      }).then(()=>{
+        return this.store.findRecord(modelName, 1)
+      }).then(()=>{
+        return this.store.query(modelName, {'tasty': true})
+      }).then(()=>{
+        return this.store.queryRecord(modelName, {'tasty': true})
+      }).then(()=>{
+        this.set('urls', this.get('urlTracker.urls'))
+        this.set('errorMessage', '')
+      })
+    } catch(e){
+      this.set('errorMessage', e.message)
+    }
   },
   init(){
     this.findUrls()
